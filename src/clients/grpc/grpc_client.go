868eeb50f0cfd971c6d1f1b0ad2a1043b7d529e2
maei <matthias.eiletz@aws-instit.de>
2020-06-02 11:36:45 +0200

    update grpc_client

--- src/clients/grpc/grpc_client.go
+++ src/clients/grpc/grpc_client.go
@@ -1,1 +1,16 @@
 package grpc
+
+import (
+	"google.golang.org/grpc"
+	"log"
+)
+
+func NewGRPCClient(url string) (*grpc.ClientConn, error) {
+	conn, err := grpc.Dial(url, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+		return nil, err
+	}
+
+	return conn, nil
+}
